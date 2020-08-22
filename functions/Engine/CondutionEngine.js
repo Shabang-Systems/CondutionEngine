8626988e981ce63be9bc5c63b51b29b5a2c7cc9b
Houjun Liu <hliu@shabang.cf>
2020-08-22 10:00:38 -0700

    Removing original functions

--- functions/Engine/CondutionEngine.js
+++
@@ -1,7 +0,0 @@
-let dbFuncs = require("./src/ObjectManager");
-let pspObj = require("./src/PerspectiveManager");
-let dbRefObj = require("./src/DBManager");
-
-
-module.exports = {start:dbRefObj.__init__, use:dbRefObj.useDb, db: dbFuncs, perspective: pspObj, flush: dbRefObj.flush};
-
