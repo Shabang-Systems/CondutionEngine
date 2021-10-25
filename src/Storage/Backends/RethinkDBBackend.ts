763612872100dd474c618d60284de66d59a7c2a4
Houjun Liu <hliu@shabang.cf>
2021-10-24 23:37:31 -0700

    I mean "working" pages
    co-authored-by: zbuster05 <zakary.saya@gmail.com>

--- src/Storage/Backends/RethinkDBBackend.ts
+++ src/Storage/Backends/RethinkDBBackend.ts
@@ -13,33 +13,98 @@ class ReThinkPage extends Page {
     private userid: string;
     private pageid: string;
     private db: string;
-
+    private working_connection;
+    
     private data: object; 
     
-    constructor(connection, db: string, userid:string, pageid:string, loadfunc: Function) {
+    constructor(path:string[], connection, refreshCallback?:Function) {
 	super();
-	this.userid = userid;
-	this.pageid = pageid;
+
+	this.db = path[0]
+	this.userid = path[1]
+	this.primarykey = path[2]
+	this.pageid = path[3]
+
+	this.working_connection = connection
 	
-	this.data = {}
+	this.path = path
+	this.data = new Promise((res,rej) => {
+	    r.db(this.db).table(this.userid).get(this.primarykey).run(connection, (err, result) => {
+		if (err) 
+		    rej(err);
+		res(Object.assign(result[this.pageid], {id: this.pageid}));
+	    });
+		
+	})
+
+	if (refreshCallback) {
+	    r.db(this.db).table(this.userid).changes().get(this.pageid).run(connection, (err, result) => {
+		if (!err)  {
+		    let r = Object.assign(result[this.pageid], {id: this.pageid, exists: true});
+		    refreshCallback(r);
+		    this.data = new Promise((res, _) => r)
+		}
+	    });
+	}
+
     }
     
     async set(payload:object, ...param:any) : Promise<DataExchangeResult>{
-	this.data = payload;
+	let data = await this.data;
+
+        if (param[0] && param[0].merge)
+            data = Object.assign(data, payload);
+        else
+            data = payload;
+
+	this.data = new Promise((res, _) => res(data));
+
+	let res = new Promise((res, _) => {
+	    // r.
+	    let query = {};
+	    query[this.primarykey][this.pageid] = data;
+	    r.db(this.db).table(this.userid).update(query).run(connection, (err, result) => {
+		res(result);
+	    });
+	});
 	
-	return {identifier: this.id, payload: payload, response: pointer}
+	return {identifier: this.id, payload: payload, response: await res}
     }
     
     async update(payload:object) : Promise<DataExchangeResult>{
-	return
+	let data = await this.data;
+
+	data = Object.assign(data, payload);
+
+	this.data = new Promise((res, _) => res(data));
+
+	let res = new Promise((res, _) => {
+	    // r.
+	    let query = {};
+	    query[this.primarykey][this.pageid] = data;
+	    r.db(this.db).table(this.userid).update(query).run(connection, (err, result) => {
+		res(result);
+	    });
+	});
+	
+	return {identifier: this.id, payload: payload, response: await res}
     }
 
     async delete() : Promise<DataExchangeResult>{
-	return
+	let res = new Promise((res, _) => {
+	    r.db(this.db)
+		.table(this.userid).get(this.pageid)
+		.delete().run(connection, (err, result) => {
+		res(result);
+	    });
+	});
+	return {identifier: null, payload: null, response: await res};
     }
 
     async exists() : Promise<boolean> {
-	return
+	// yes, I really did this.
+	// no, I am not ashamed of it.
+	return true;
     }
 
     get id() : string {
@@ -47,7 +112,8 @@ class ReThinkPage extends Page {
     }
 
     async get() : Promise<object> {
-	
-	return
+	return await self.data;
     }
 }
+
+export { ReThinkPage }
--- tests/scratchpad.ts
+++ tests/scratchpad.ts
@@ -1,1 +1,14 @@
 import "../src/CondutionEngine";
+import {ReThinkPage} from "../src/Storage/Backends/RethinkDBBackend.ts";
+
+let r = require('rethinkdb')
+var connection = null;
+r.connect( {host: 'localhost', port: 28015}, function(err, conn) {
+    if (err) throw err;
+    connection = conn;
+
+    let page = new ReThinkPage(["users", "testuserid", "tasks", "hewoooooo"], connection)
+})
+
+
+
